70e4f4e7af00acbf70f4f8ecb2e448563d0eea89
Iman Jawad <imanj12@gmail.com>
2018-07-16 17:57:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,18 +10,26 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
+<<<<<<< HEAD
   var i = 0
   while (i < facts.length) {
     facts[i] = facts[i] + '!!!'
     i++
   }
   return facts
+=======
+  
+>>>>>>> f07a8575afd278f6621e308ec42b85f3a0d303d8
 }
 
 function iLoveTheBeatles(n) {
   var tempArray = []
   do {
+<<<<<<< HEAD
     tempArray = [...tempArray, 'I love the Beatles!']
+=======
+    tempArray = [...tempArray, 'I Love the Beatles!']
+>>>>>>> f07a8575afd278f6621e308ec42b85f3a0d303d8
     n++
   } while (n < 15)
   return tempArray
